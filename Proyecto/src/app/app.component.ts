be7e8bad73f0f581eca4edd4713cf50f297a30f6
CarlosMartinNav <carlos.martinnavarro@cieep.com>
2025-03-31 16:56:43 +0200

    cambio2

--- Proyecto/src/app/app.component.ts
+++ Proyecto/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { HeaderComponent } from "./componentes/header/header.component";
+import { FooterComponent } from './componentes/footer/footer.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Proyecto/src/app/app.routes.ts
+++ Proyecto/src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { HomeComponent } from './componentes/home/home.component'
+import { MenuComponent } from './componentes/menu/menu.component'
+import { NavbarComponent } from './componentes/navbar/navbar.component'
+import { HeaderComponent } from './componentes/header/header.component'
+import { FooterComponent } from './componentes/footer/footer.component'
+import { ContactoComponent } from './componentes/contacto/contacto.component'
+import { QuienesSomosComponent } from './componentes/quienes-somos/quienes-somos.component'
+import { LoginComponent } from './componentes/login/login.component'
+import { RegistrarComponent } from './componentes/registrar/registrar.component'
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path : 'home', component: HomeComponent},
+    {path : 'menu', component: MenuComponent},
+    {path : 'navbar', component: NavbarComponent},
+    {path : 'header', component: HeaderComponent},
+    {path : 'footer', component: FooterComponent},
+    {path : 'contacto', component: ContactoComponent},
+    {path : 'quienessomos', component: QuienesSomosComponent},
+    {path : 'login', component: LoginComponent},
+    {path : 'registrar', component: RegistrarComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
